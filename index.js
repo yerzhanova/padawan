2ac7612b1b8fb2bb0a9b166b419a8b6e24c54132
Anel <a.yerzhanova@devir.kz>
2020-10-23 18:48:17 +0600

    animation

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-init(){
+
+function init(){
 	let start = new Date();
-	
+	let timer = setInterval(function(){
+		let timePassed = new Date() - start;
+		if (timePassed > 2000) {
+			clearInterval(timer); //end after 2 sec
+			return;
+		}
+		drawMonk(timePassed);
+	}, 0);
+	function drawMonk(timePassed) {
+		monk.style.top = timePassed/50 + 'px';
+	}
 }
+init();
