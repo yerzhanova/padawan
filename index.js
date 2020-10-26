94e4b93071399bac78aed9e502840520851c8ba1
Anel <a.yerzhanova@devir.kz>
2020-10-27 02:25:24 +0600

    using @keyframes

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
 let positionX = 0;
 let currentPage = 0;
 function init(){
-	let start = new Date();
-	let currentPage = 0;
+	// let start = new Date();
+	// let currentPage = 0;
 	animateText();
-	let timer = setInterval(function(){
-		let timePassed = new Date() - start;
-		if (timePassed > 3000) {
-			clearInterval(timer); //end after 2 sec
-			monk.style.display = 'none';
-			message.innerHTML = '';
-			showWrapper();
-			return;
-		}
-		jumpMonk(timePassed);
-	}, 20);
-	
-	function jumpMonk(timePassed) {
-		if (timePassed < 447) {
-			monk.style.height = timePassed / 3 + 'px';
-			if (timePassed > 298) {
-				message.style.fontSize = timePassed / 24 + 'px'
-			}
-		} else if (timePassed > 2000) {
-			// monk.style.height = 149 - timePassed/150 + 'px';
-		}
-		monk.style.top = timePassed/50 + 'px';
-	}
-	
-	function showWrapper(){
-		// if (currentPage === 0) {
-		// 	console.log(document.getElementsByClassName('li'));
+	// let monk = document.querySelector('#monk');
+	// console.log("animate");
+	// monk.animate([{"height": "0px"}, {"height": "149px"}], {"duration": 500});
+	// let timer = setInterval(function(){
+	// 	let timePassed = new Date() - start;
+	// 	if (timePassed > 3000) {
+	// 		clearInterval(timer); //end after 2 sec
+			// monk.style.display = 'none';
+			// message.innerHTML = '';
+			// showWrapper();
+			// return;
 		// }
-		console.log('wrapper is visible now');
-		wrapper.style.display = 'block';
-		document.body.style.backgroundPosition = '0% 0%';
-		document.body.style.backgroundImage = "url('./assets/images/background.jpg')";
-		document.body.style.backgroundRepeat = 'no-repeat';
-		footer.style.display = 'block';
-	}
+		// jumpMonk(timePassed);
+	// }, 20);
+	
+	// function jumpMonk(timePassed) {
+	// 	if (timePassed < 447) {
+	// 		monk.style.height = timePassed / 3 + 'px';
+	// 		if (timePassed > 298) {
+	// 			message.style.fontSize = timePassed / 24 + 'px'
+	// 		}
+	// 	} else if (timePassed > 2000) {
+	// 		// monk.style.height = 149 - timePassed/150 + 'px';
+	// 	}
+	// 	monk.style.top = timePassed/50 + 'px';
+	// }
+	
+	// function showWrapper(){
+	// 	// if (currentPage === 0) {
+	// 	// 	console.log(document.getElementsByClassName('li'));
+	// 	// }
+	// 	console.log('wrapper is visible now');
+	// 	wrapper.style.display = 'block';
+	// 	document.body.style.backgroundPosition = '0% 0%';
+	// 	document.body.style.backgroundImage = "url('./assets/images/background.jpg')";
+	// 	document.body.style.backgroundRepeat = 'no-repeat';
+	// 	// footer.style.display = 'block';
+	// }
 	
 }
 function animateText() {
@@ -49,7 +52,11 @@ function animateText() {
 	setTimeout(() => {messageInner.innerHTML = messageInner.innerHTML.slice(0, -1) + (message2)}, 1500);
 }
 function clickRight() {
-	console.log(currentPage);
+    console.log(currentPage);
+    let f = document.body;
+    console.log(f, "body");
+	f.animate([{"background-position": "0px"}, {"background-position": "200px"}], {"duration": 1000});
+
 	if (currentPage === 0) {
 		positionX += 15;
 	} else if (currentPage >= 1 && currentPage < 8) {
@@ -62,7 +69,7 @@ function clickRight() {
 	}
 	currentPage++;
 	console.log('move to the right', currentPage);
-	document.body.style.backgroundPosition = positionX+'%';
+	// document.body.style.backgroundPosition = positionX+'%';
 }
 function clickLeft(){
 	if (currentPage === 1){
