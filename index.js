2950e207e29319b09d09ed56b2ce1fd454e4326f
Anel <a.yerzhanova@devir.kz>
2020-10-29 16:03:43 +0600

    page title text animation

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function init(){
     ' wisdom are shared', 'train for perfection but aim for more', 'take pride in your work but do not seek praize', 'temporary sacrifice brings lasting results' ];
     const pagination = document.getElementById('navigate').children;
     let currentPage = 0;
-	const right = document.getElementById('arrowRight').getElementsByTagName('p')[0];
-	pagination[currentPage].style.class = 'active';
+	// const right = document.getElementById('arrowRight').getElementsByTagName('p')[0];
+
 	const pages = document.getElementById('navigate').getElementsByTagName('li');
 	const footerMessage = document.querySelector('.footerText > p');
 	const footerText = document.querySelectorAll("div.footer > p");
@@ -16,6 +16,7 @@ function init(){
     const rightTooltip = document.getElementById('rightTooltip');
     const pagesPosition = [0, 8, 23, 37, 51, 65, 80, 100, 100, 115];
 	animateText();
+	loadPage(0);
     document.body.addEventListener('animationend', function(){
         document.body.style.backgroundImage = "url('./assets/images/background.jpg')";
         // document.body.style.backgroundPosition = "0%";
@@ -24,11 +25,11 @@ function init(){
     document.getElementById('arrowRight').addEventListener('click', (e) => {
 		if (currentPage < 9)
 			loadPage(currentPage + 1);
-	});
+    });
     document.getElementById('arrowLeft').addEventListener('click', (e) => {
     	if (currentPage > 0)
         	loadPage(currentPage - 1);
-	});
+    });
 	for (let i=0; i < pages.length; i++){
 		pages[i].querySelector('div').addEventListener('click', (e) => {
 			loadPage(i);
@@ -45,7 +46,16 @@ function init(){
         // leftTooltip.innerHTML = ((pageNumber > 0 && pageNumber < 3) || (pageNumber > 5 && pageNumber < 9))?tooltips[currentPage]:'';
         animateEvents(footerMessage, 'show');
         document.body.style.backgroundPosition = pagesPosition[pageNumber] + "%";
-
+		if (pageNumber === 0) {
+			leftTooltip.style.opacity = 1;
+		} else if (currentPage === 0) {
+			leftTooltip.style.opacity = 0;
+		}
+		if (pageNumber === 9) {
+			rightTooltip.style.opacity = 1;
+		} else if (currentPage === 9) {
+			rightTooltip.style.opacity = 0;
+		}
         lastPage = pageNumber;
         currentPage = pageNumber;
 	}
@@ -60,7 +70,9 @@ function init(){
         //if element has not className
         element.classList.remove(className);
         window.requestAnimationFrame( function () {
+        	console.log("2222");
             element.classList.add(className);
+            console.log('3333');
         });
     }
 }
